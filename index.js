e5b6720a8f5be3c3e34a83819f2d1e24d1855bb5
owlsoverjericho <owlsoverjericho@protonmail.com>
2023-08-08 13:27:42 +0300

    tested git diff

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import http from 'node:http'
 
-const PORT = 8000;
 
 const server = http.CreateServer((req, res) => {
 	res.end()
 });
 
+const PORT = 8000;
+
 server.listen(PORT, () => console.log(`The server is listening on port: ${PORT}`));
+
+console.log(`Added some changees to test the git diff option`)
